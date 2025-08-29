6d1c4dd3381eabc51c917ba1104215f072ccdfdf
Othman Morad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-29 20:57:12 +0300

    Add files via upload

---
+++ problem#10 course5.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do {
+        cout << message << endl;
+        cin >> number;
+    } while (number < 0);
+
+    return number;
+}
+int ReverseNumber(int number)
+{
+    int Remainder = 0;
+    int number2 = 0;
+
+    while (number > 0)
+    {
+        Remainder = number % 10;
+        number = number / 10;
+        number2 = number2 * 10 + Remainder;
+    }
+
+    return number2;
+}
+
+void PrintDigits(int number)
+{
+    int Remainder = 0;
+   
+    while (number > 0)
+    {
+        Remainder = number % 10;
+        number = number / 10;
+        cout << Remainder << endl;
+    }
+
+}
+int main()
+{
+
+    PrintDigits(ReverseNumber(ReadPositiveNumber("Please enter a positive number ?")));
+
+}

6cb52cc7acb177f3f431b38a481dd3775aacfc83
Othman Morad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-29 21:26:10 +0300

    Add files via upload

---
+++ problem#44 course5.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int RandoomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+void FillArrayByRandoomNumber(int arr[100], int& arrLength)
+{
+    cout << "Enter Number of element : " << endl;
+    cin >> arrLength;
+    for (int i = 0;i < arrLength;i++)
+        arr[i] = RandoomNumber(-100, 100);
+    cout << "\n";
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0;i < arrLength;i++)
+        cout << arr[i] << " ";
+}
+
+int PositiveCount(int arr[100], int& arrLength)
+{
+    int counter = 0;
+
+    for (int i = 0;i < arrLength;i++)
+
+    {
+        if (arr[i] >= 0 )
+        {
+            counter++;
+        }
+    }
+
+    return counter;
+
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    int arrLength = 0;
+
+    FillArrayByRandoomNumber(arr, arrLength);
+
+
+    cout << "\nArray 1 Elements : ";
+    PrintArray(arr, arrLength);
+
+    cout << "\nPositive Numbers count is : ";
+
+    cout << PositiveCount(arr, arrLength) << endl;
+
+    return 0;
+}

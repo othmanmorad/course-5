11d77c411d75f39002f240801b44cb7f850a07ea
Othman Morad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-29 21:00:01 +0300

    Update problem#12 course5.cpp

--- problem#12 course5.cpp
+++ problem#12 course5.cpp
@@ -1,11 +1,37 @@
+
 #include <iostream>
 #include<string>
 using namespace std;
 
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do {
+        cout << message << endl;
+        cin >> number;
+    } while (number < 0);
 
+    return number;
+}
+
+void PrintInvertedNumberPattern(int Number)
+{
+    for (int i = Number; i >= 1; i--)
+    {
+
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    PrintInvertedNumberPattern(ReadPositiveNumber("Please enter a positive number "));
+    return 0;
 }
+
 

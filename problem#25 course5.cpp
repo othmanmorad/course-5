0b40665be23e6bb1a372596b275d23753a05d17f
Othman Morad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-29 21:08:30 +0300

    Add files via upload

---
+++ problem#25 course5.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int RandoomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+void FillArrayByRandoomNumber(int arr[100], int& Length)
+{
+    cout << "Enter Number of element : " << endl;
+    cin >> Length;
+    for (int i = 0;i <= Length;i++)
+        arr[i] = RandoomNumber(1, 100);
+    cout << "\n";
+}
+
+void PrintArray(int arr[100], int Length)
+{
+    for (int i = 0;i <= Length;i++)
+        cout << arr[i] << " ";
+}
+int MinNumberInArray(int arr[100], int Length)
+{
+
+    int Min = arr[0];
+    for (int i = 0;i <= Length;i++)
+    {
+        if ( arr[i]<Min)
+        {
+            Min = arr[i];
+        }
+    }
+
+    return Min;
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    int Length;
+
+    FillArrayByRandoomNumber(arr, Length);
+
+    cout << "\nArray Elements : ";
+    PrintArray(arr, Length);
+
+
+    cout << "\nMin number is :  ";
+    cout << MinNumberInArray(arr, Length);
+    return 0;
+}
+
+
